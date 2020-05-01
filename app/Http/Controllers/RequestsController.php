4cc2fbd1956d5827e74525b811d0627fc5a82dd6
oferraro <oscar@conan-ui.com>
2020-05-01 14:25:44 -0300

    Reorder in methods, check if params are valid

--- app/Http/Controllers/RequestsController.php
+++ app/Http/Controllers/RequestsController.php
@@ -23,41 +23,40 @@ class RequestsController extends Controller
                 $l = explode(" ", $line);
 
                 // TODO: check if it's an IP or a host to set the array/json key
-                $time = explode(':', $l[1]);
                 $request = explode(' ', $quoted);
-
-                if (count($request) > 1) { // has also method
-                    $protocols = (isset($request[2]) ? explode('/', $request[2]) : '');
-                    $requestData = [
-                        'method' => $request[0],
-                        'url' => isset($request[1]) ? $request[1] : '',
-                        'protocol' => isset($protocols[0]) ? $protocols[0] : $protocols,
-                        'protocol_version' => isset($protocols[1]) ? $protocols[1] : ''
-                    ];
-                }
+                $requestData = $this->parseProtocol($request);
 
                 $newArr = [
                     'host' => $l[0],
-                    'datetime' => [
-                        'day' => str_replace('[', '', $time[0]),
-                        'hour' => $time[1],
-                        'minute' => $time[2],
-                        'second' => str_replace(']', '', $time[3])
-                    ],
+                    'datetime' => $this->parseTimeData($l[1]),
                     'request' => isset($requestData) ? $requestData : '',
                     'response_code' => (isset($l[5]) ? $l[5] : ''),
                     'document_size' => (isset($l[6]) ? $l[6] : '')
                 ]; // TODO: add more fields, there are no more fields in current files format
                 $results[] = $newArr;
             }
+
             $limit = $urlRequest->get('limit') ? $urlRequest->get('limit') : 100;
             $offset = $urlRequest->get('offset') ? $urlRequest->get('offset') : 0;
             $total = count($results);
 
             $limitedResults = [];
-            for ($i = $offset; $i <= ($offset + $limit); $i++) {
+
+            if (
+                $total < ($offset + $limit)
+                || ($total <= $offset)
+            ) {
+                return response()->json([
+                    'success' => 'false',
+                    'error' => 'error in parameters'
+                ]);
+            }
+
+
+            for ($i = $offset; $i < ($offset + $limit); $i++) {
                 $limitedResults[] = $results[$i];
             }
+
             return response()->json([
                 'success' => true,
                 'limit' => $limit,
@@ -76,4 +75,25 @@ class RequestsController extends Controller
         }
     }
 
+    private function parseProtocol($request) {
+        $protocols = (isset($request[2]) ? explode('/', $request[2]) : '');
+        $requestData = [
+            'method' => $request[0],
+            'url' => isset($request[1]) ? $request[1] : '',
+            'protocol' => isset($protocols[0]) ? $protocols[0] : $protocols,
+            'protocol_version' => isset($protocols[1]) ? $protocols[1] : ''
+        ];
+        return $requestData;
+    }
+
+    private function parseTimeData($timeData){
+        $time = explode(':', $timeData);
+        $returnData = [
+            'day' => str_replace('[', '', $time[0]),
+            'hour' => $time[1],
+            'minute' => $time[2],
+            'second' => str_replace(']', '', $time[3])
+        ];
+        return $returnData;
+    }
 }
